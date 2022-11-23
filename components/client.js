e786d4a293e15605a0e8ff859f8a376f1bab0ac6
dprama <rampradios@gmail.com>
2022-11-23 19:56:54 +0800

    add apose

--- components/client.js
+++ components/client.js
@@ -4,7 +4,7 @@ export function Client() {
     return (
         <div>
 <div class="py-16 px-4 mx-auto bg-white">
-  <h4 class="text-4xl font-semibold text-center mb-10">Clients we've worked with</h4>
+  <h4 class="text-4xl font-semibold text-center mb-10">Clients we have worked with</h4>
   <Image class="mx-auto" src="/images/clients.jpg" width={800} height={120} />
 </div>
 
